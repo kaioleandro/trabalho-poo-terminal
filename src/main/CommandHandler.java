dcdc3f059cb175499741f91f590b65922d03f647
Erik de Oliveira Padua <erikopadua@outlook.com>
2026-01-20 14:32:43 -0300

    feat: atualiza CommandHandler para gerenciar os comandos

--- src/main/CommandHandler.java
+++ src/main/CommandHandler.java
@@ -1,4 +1,35 @@
 package main;
 
+import main.command.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandHandler {
+    private final Map<String, Command> mappedCommands = new HashMap<>();
+
+    public CommandHandler() {
+        mapCommand(new CommandPwd());
+        mapCommand(new CommandExit());
+        mapCommand(new CommandMkdir());
+        mapCommand(new CommandCd());
+    }
+
+    private void mapCommand(Command command) {
+        mappedCommands.put(command.name(), command);
+    }
+
+    public void catchCommand(String input) {
+        String[] args = input.trim().split("\\s+");
+
+        Command command = mappedCommands.get(args[0]);
+
+        if(command == null){
+            System.out.println("Comando inválido");
+            return;
+        }
+
+        command.execute(args);
+    }
 }
+
