11b2834bc58d7c656d328ee955c84105562a85c8
Erik de Oliveira Padua <erikopadua@outlook.com>
2026-01-20 14:37:46 -0300

    feat: atualiza a classe Terminal (main)
        - Adiciona o método consoleListener para receber entradas do console
        - Integra a classe ao CommandHandler responsável pela execução dos comandos

--- src/main/Terminal.java
+++ src/main/Terminal.java
@@ -1,6 +1,9 @@
 package main;
 
+import java.util.Scanner;
+
 public class Terminal {
+
 
     public static void main(String[] args) {
         start();
@@ -8,9 +11,34 @@ public class Terminal {
 
     private static void start() {
         PathManager pathManager = PathManager.getInstance();
+        String rootDirectory = "home/user";
 
-        DirectoryManager.createDirectory(pathManager.getCurrentPath().resolve("home"));
-        pathManager.changeCurrentPath("home");
+        DirectoryManager.createDirectory(pathManager.getCurrentPath().resolve(rootDirectory));
+        pathManager.changeCurrentPath(rootDirectory);
 
+        consoleListener(pathManager, rootDirectory);
+    }
+
+    private static void consoleListener(PathManager pathManager, String rootDirectory) {
+        CommandHandler commandHandler = new CommandHandler();
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            try {
+                String path = pathManager.getCurrentPath().toString();
+                int index = path.indexOf(rootDirectory);
+
+                System.out.print(path
+                        .substring(index + rootDirectory.length())
+                        .concat("> ")
+                );
+
+                commandHandler.catchCommand(input.nextLine());
+            } catch (Exception e) {
+                input.close();
+                break;
+            }
+        }
     }
 }
+
