485d31b190642e686349acfd609c53ca02be3032
Kaio Leandro <93662668+kaioleandro@users.noreply.github.com>
2026-01-20 21:08:23 -0300

    feat: adicionado criador de arquivos em fileManager

--- src/main/FileManager.java
+++ src/main/FileManager.java
@@ -1,4 +1,20 @@
 package main;
 
-public class FileManager {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManager implements FileSystemUtils {
+    public static void CreateFile(Path path) {
+        try {
+            if (Files.exists(path)) {
+                System.out.println("Arquivo já existe");
+                return;
+            }
+
+            Files.createFile(path);
+        } catch (IOException e) {
+            System.out.println("Erro ao criar arquivo");
+        }
+    }
 }
